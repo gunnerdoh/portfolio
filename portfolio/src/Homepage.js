37dc690f3e3a2fc22d1743d0d6f119654da13263
gunnerdoh <gunnerdoh2@gmail.com>
2024-11-20 12:11:50 -0800

    finishing up

--- portfolio/src/Homepage.js
+++ portfolio/src/Homepage.js
@@ -3,60 +3,74 @@ import Mountain from './components/mountain';
 import Pageload from './components/Pageload';
 import Contact from './components/Contact';
 
+function dateString() {
+    var currentdate = new Date();
+    var date = currentdate.getDay() + "." + currentdate.getMonth() + "." + currentdate.getFullYear();
+    return (
+        <div>
+            <p className="font-raleway text-xl text-gray-300">Click a word on the left to see more.</p>
+            <p className="font-raleway text-xl text-gray-300">{date} ~</p>
+        </div>
+    );
+}
+
 function Homepage() {
     const [isWorkVisible, setIsWorkVisible] = useState(false);
     const [isWorkBold, setIsWorkBold] = useState(false);
-
     const [isElseVisible, setIsElseVisible] = useState(false);
     const [isElseBold, setIsElseBold] = useState(false);
-
     const [isAboutVisible, setIsAboutVisible] = useState(false);
     const [isAboutBold, setIsAboutBold] = useState(false);
-
-    const [isDarkMode, setIsDarkMode] = useState(false);
 
     const handleWorkClick = () => {
         setIsWorkVisible(!isWorkVisible);
         setIsWorkBold(!isWorkBold);
+        
+        // setIsElseVisible(false);
+        // setIsElseBold(false);
+
+        // setIsAboutVisible(false);
+        // setIsAboutBold(false);
     };
 
     const handleElseClick = () => {
         setIsElseVisible(!isElseVisible);
         setIsElseBold(!isElseBold);
+
+        // setIsAboutVisible(false);
+        // setIsAboutBold(false);
+
+        // setIsWorkVisible(false);
+        // setIsWorkBold(false);
     };
 
     const handleAboutClick = () => {
         setIsAboutVisible(!isAboutVisible);
         setIsAboutBold(!isAboutBold);
-    };
 
-    const toggleDarkMode = () => {
-        setIsDarkMode(!isDarkMode);
+        // setIsElseVisible(false);
+        // setIsElseBold(false);
+
+        // setIsWorkVisible(false);
+        // setIsWorkBold(false);
     };
 
     return (
-        <div className={`${isDarkMode ? 'bg-gray-900' : 'bg-original-stone'} transition-colors duration-300`}>
+        <div className="bg-original-stone">
             <Pageload />
             {/* Header and contact icons */}
             <div className='min-h-screen px-20 py-10 ps-30'>
                 <div className='flex flex-row justify-between mb-5'>
                     <div>
-                        <h2 className={`font-averia underline underline-offset-2 text-2xl font-bold ${isDarkMode ? 'text-white' : 'text-black'}`}>
+                        <h2 className="font-averia underline underline-offset-2 text-2xl font-bold text-black">
                             Gunner Dohrenwend
                         </h2>
-                        <p className={`"font-averia text-l font-light text-gray" ${isDarkMode ? 'text-white' : 'text-black'}`}>
+                        <p className="font-averia text-l font-light text-black">
                             gunnerdoh@gmail.com
                         </p>
                     </div>
                     <div className="flex flex-row justify-end items-center">
                         <Contact />
-                        <button 
-                            onClick={toggleDarkMode}
-                            className={`"ml-5 p-2 rounded-full border-2 border-black 
-                             transition-colors
-                            ${isDarkMode ? 'hover:bg-gray-200 dark:hover:bg-gray-100' : 'hover:bg-gray-700 dark:hover:bg-gray-700'}"`}
-                        >
-                        </button>
                     </div>
                 </div>
                 {/* Other stuff */}
@@ -64,96 +78,99 @@ function Homepage() {
                     <div className="flex flex-col align-left w-1/2">
                         <h2 
                             onClick={handleAboutClick}
-                            className={`font-raleway text-2xl cursor-pointer transition-all
-                                ${isDarkMode ? 'text-white hover:text-gray-300' : 'text-black hover:text-gray-700'}
+                            className={`font-raleway text-2xl text-black cursor-pointer hover:text-gray-700 transition-all
                                 ${isAboutBold ? 'font-bold' : 'font-light'}`}
                         >
                             ABOUT
                         </h2>
-
                         <h2 
                             onClick={handleWorkClick}
-                            className={`font-raleway text-2xl cursor-pointer transition-all
-                                ${isDarkMode ? 'text-white hover:text-gray-300' : 'text-black hover:text-gray-700'}
+                            className={`font-raleway text-2xl text-black cursor-pointer hover:text-gray-700 transition-all
                                 ${isWorkBold ? 'font-bold' : 'font-light'}`}
                         >
                             WORK + PROJECTS
                         </h2>
-                        <h2 className={`font-raleway text-2xl font-light ${isDarkMode ? 'text-white' : 'text-black'}`}>RESUME</h2>
-                        <h2 
-                            onClick={handleElseClick}
-                            className={`font-raleway text-2xl cursor-pointer transition-all
-                                ${isDarkMode ? 'text-white hover:text-gray-300' : 'text-black hover:text-gray-700'}
-                                ${isElseBold ? 'font-bold' : 'font-light'}`}
-                        >
-                            EVERYTHING ELSE
-                        </h2>
+                        <a href="./resume.pdf" className="font-raleway text-2xl font-light text-black " target="_blank" rel="noopener noreferrer">
+                            RESUME
+                        </a> 
                         <Mountain />
                     </div>
-                    <p className="m-auto text-3xl font-raleway"></p>
+                    <p className="p-auto m-auto text-center text-3xl font-raleway"></p>
                 </div>
+
                 {/* About Panel */}
-                <div 
-                    className={`
-                        fixed top-0 right-0 h-full w-[61rem] shadow-lg p-8
-                        transform transition-all duration-500 ease-in-out font-averia
-                        ${isDarkMode ? 'bg-gray-800 border-l-2 border-gray-700' : 'bg-gray-200 border-l-2 border-black'}
-                        ${isAboutVisible ? 'translate-x-0' : 'translate-x-full'}
-                    `}
-                >
+                <div className={`
+                    fixed top-0 right-0 h-full w-[61rem] shadow-lg p-8
+                    bg-gray-200 border-l-2 border-black
+                    font-averia
+                    ${isAboutVisible ? 'translate-x-0' : 'translate-x-full'}
+                `}>
+                    <h2 className="text-2xl flex-none font-bold mb-4 text-gray-800">
+                        About
+                    </h2>
                     <div className="flex flex-row">
-                        <h2 className={`text-2xl flex-none font-bold mb-4  ${isDarkMode ? 'text-white' : 'text-gray-800'}`}>
-                            About
-                        </h2>
-                        <div className="text-right flex-grow pr-4">
-
-                        </div>
-                    </div>
-                    <div>
-                        <p>
-                            Hi, I'm Gunner - an Informatics Student at the University of Washington. 
-                        </p>
-                        <p>
-                            I focus mostly on software development and data analysis, and am passionate about
-                            using data to solve complex problems and drive innovation in the tech industry. 
-                        </p>
-                        <div>
-
+                        <div className="w-2/3">
+                            <p className="mb-5">
+                                Hi, I'm Gunner - an Informatics Student at the University of Washington. 
+                            </p>
+                            <p className="mb-5">
+                                My studies have focused mostly on software engineering and data science. 
+                                I plan to build a career in these fields and design great systems for information
+                                management. 
+                            </p>
+                            <p className="mb-5">
+                                My studies in the last few years have been diverse, including ML Pipeline Development,
+                                ethnographic study, web development, computer architechture, DB management, and 
+                                Data Structures. 
+                            </p>
                         </div>
                     </div>
                 </div>
                 
-                {/* Work + School Panel */}
-                <div 
-                    className={`
-                        fixed top-0 right-0 h-full w-[60rem] shadow-lg p-8
-                        transform transition-all duration-500 ease-in-out font-averia
-                        ${isDarkMode ? 'bg-gray-800 border-l-2 border-gray-600' : 'bg-white border-l-2 border-black'}
-                        ${isWorkVisible ? 'translate-x-0' : 'translate-x-full'}
-                    `}
-                >
+                {/* Work Panel */}
+                <div className={`
+                    fixed top-0 right-0 h-full w-[60rem] shadow-lg p-8
+                    bg-white border-l-2 border-black
+                     font-averia
+                    ${isWorkVisible ? 'translate-x-0' : 'translate-x-full'}
+                `}>
+                    <h2 className="text-2xl flex-none font-bold mb-4 text-gray-800">
+                        Work + Projects
+                    </h2>
                     <div className="flex flex-row">
-                        <h2 className={`text-2xl flex-none font-bold mb-4  ${isDarkMode ? 'text-white' : 'text-gray-800'}`}>
-                            Work
-                        </h2>
-                        <h2 className="text-right flex-grow pr-4">Contact</h2>
-                    </div>
-                </div>
-
-                {/* Everything Else Panel */}
-                <div 
-                    className={`
-                        fixed top-0 right-0 h-full w-[59rem] shadow-lg p-8
-                        transform transition-all duration-500 ease-in-out font-averia
-                        ${isDarkMode ? 'bg-gray-700 border-l-2 border-gray-600' : 'bg-gray-300 border-l-2 border-black'}
-                        ${isElseVisible ? 'translate-x-0' : 'translate-x-full'}
-                    `}
-                >
-                    <div className="flex flex-row">
-                        <h2 className={`text-2xl flex-none font-bold mb-4  ${isDarkMode ? 'text-white' : 'text-gray-800'}`}>
-                            Everything Else
-                        </h2>
-                        <h2 className="text-right flex-grow pr-4">Contact</h2>
+                        <div className="w-2/3">
+                            <h3 className="mb-5 text-xl">2024</h3>
+                            <h4 className="mb-3 font-bold text-black">
+                                Axiom Health - Software Engineering Intern
+                            </h4>
+                            <p>
+                                The needs of the company shifted between me being initially hired and
+                                the start of my internship, so I pivoted to more of a data science role.
+                                Most of my work was in developing automated data collection to improve
+                                information intake for the company's tuned LLM and finding data insights 
+                                in Databricks using PySpark and PySQL for analysis and Matplotlib for visualization. 
+                            </p>
+                            <h3 className="my-5 text-xl">2023</h3>
+                            <h4 className="mb-3 font-bold text-black">
+                                Coding 4 Youth - Computer Science Instructor
+                            </h4>
+                            <p>
+                                While simultaneously taking classes, I taught up to 11 students a week remotely on
+                                computer science fundamentals, involving tools such as Python, JS, HTML / CSS, 
+                                Scratch, and even a class on Minecraft Redstone. 
+                            </p>
+                            <h4 className="mb-3 mt-5 font-bold text-black">
+                                Student Researcher - University of Washington iSchool 
+                            </h4>
+                            <p>
+                                I spent a month in Addis Ababa, Ethiopia as part of an independent 
+                                design study researching prototypes to improve tech education in Addis Ababa.
+                                You can find my write-up 
+                                <a href="./Addis_Ababa_iSchool_Writeup.pdf" className="text-blue-600 ps-1 " target="_blank" rel="noopener noreferrer">
+                                 here
+                                </a>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
